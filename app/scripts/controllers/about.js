ec331bdf0fb72cdb7927a953d79f39d2c6576ce1
Laura Ambrose <lauraambrose@gmail.com>
2017-05-03 19:56:05 +0800

    added about page detailing test spec

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieLibraryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the movieLibraryApp
+ */
+angular.module('movieLibraryApp')
+  .controller('AboutCtrl', function () {
+
+  });
